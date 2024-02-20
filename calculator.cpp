2fd186a4f4583202558c16175aec98fe75753c90
Sebuunia <88202799+Sebuunia@users.noreply.github.com>
2024-02-20 21:30:56 +0100

    yes

--- calculator.cpp
+++ calculator.cpp
@@ -4,38 +4,38 @@ using namespace std;
 
 int main()
 {
-char operacja;
-float num1;
-float num2;
+    char operacja;
+    float num1;
+    float num2;
 
-cout << "Podaj pierwsza liczbe: ";
-cin >> num1;
-cout << "Podaj druga liczbe: ";
-cin >> num2;
+    cout << "Podaj pierwsza liczbe: ";
+    cin >> num1;
+    cout << "Podaj druga liczbe: ";
+    cin >> num2;
 
-cout << "Wybierz operator: +, -, *, /: ";
-cin >> operacja;
+    cout << "Wybierz operator: +, -, *, /: ";
+    cin >> operacja;
 
-switch (operacja) {
-case '+':
-    cout << "Wynik: " << num1 + num2;
-    break;
-case '-':
-    cout << "Wynik: " << num1 - num2;
-    break;
-case '*':
-    cout << "Wynik: " << num1 * num2;
-    break;
-case '/':
-    if (num2 != 0)
-        cout << "Wynik: " << num1 / num2;
-    else
-        cout << "Dzielenie przez zero!";
-    break;
-default:
-    cout << "Nieprawidlowy operator!";
-    break;
-}
+    switch (operacja) {
+    case '+':
+        cout << "Wynik: " << num1 + num2;
+        break;
+    case '-':
+        cout << "Wynik: " << num1 - num2;
+        break;
+    case '*':
+        cout << "Wynik: " << num1 * num2;
+        break;
+    case '/':
+        if (num2 != 0)
+            cout << "Wynik: " << num1 / num2;
+        else
+            cout << "Dzielenie przez zero!";
+        break;
+    default:
+        cout << "Nieprawidlowy operator!";
+        break;
+    }
 
-return 0;
+    return 0;
 }
